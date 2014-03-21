b2921b9c6830bb391324a1152fb7917cb65afb79
Nguyen Anh Tuan <tuanit09@gmail.com>
2014-03-21 21:46:53 +0700

    ITViewFramework
    Framework to work with layout

---
+++ UIView+ITViewUtils.h
@@ -0,0 +1,13 @@
+//
+//  UIView+ITViewUtils.h
+//  CloudReader
+//
+//  Created by Tuan Anh Nguyen on 3/21/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ITViewUtils)
+
+@end
---
+++ UIView+ITViewUtils.m
@@ -0,0 +1,13 @@
+//
+//  UIView+ITViewUtils.m
+//  CloudReader
+//
+//  Created by Tuan Anh Nguyen on 3/21/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import "UIView+ITViewUtils.h"
+
+@implementation UIView (ITViewUtils)
+
+@end
